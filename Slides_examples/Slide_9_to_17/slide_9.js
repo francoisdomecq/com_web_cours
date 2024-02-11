9424c6b5d4e599990477ac7219b99370960038c4
francoisdmq <francois.domecq@ratp.fr>
2024-02-11 11:02:31 +0100

    slides examples

---
+++ Slides_examples/Slide_9_to_17/slide_9.js
@@ -0,0 +1,5 @@
+const titleElements = document.getElementsByTagName("h2");
+
+const titleElements = document.getElementsByClassName("title");
+
+const titleElement = document.getElementById("titre_principal");
